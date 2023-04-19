7027945c2fcd8fecc4fc1d4c3a2533b023818609
mattgwin@udel.edu <mattgwin@udel.edu>
2023-04-19 16:12:34 -0700

    Fix compilation issue

--- image.c
+++ image.c
@@ -56,11 +56,10 @@ uint8_t getPixelValue(Image* srcImage,int x,int y,int bit,Matrix algorithm){
 //            destImage: A pointer to a  pre-allocated (including space for the pixel array) structure to receive the convoluted image.  It should be the same size as srcImage
 //            algorithm: The kernel matrix to use for the convolution
 //Returns: Nothing
-#omp parallel
 void convolute(Image* srcImage,Image* destImage,Matrix algorithm){
     int row,pix,bit,span;
     span=srcImage->bpp*srcImage->bpp;
-    #pragma omp for
+    #pragma omp parallel for
     for (row=0;row<srcImage->height;row++){
         for (pix=0;pix<srcImage->width;pix++){
             for (bit=0;bit<srcImage->bpp;bit++){
